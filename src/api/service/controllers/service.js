e5809f89891622313084fbcc61bdc83018ad9e15
Gunter Gielen <gunter.gielen@telenet.be>
2023-01-31 09:15:28 +0100

    wrong slug system reverted

--- src/api/service/controllers/service.js
+++ src/api/service/controllers/service.js
@@ -6,12 +6,4 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::service.service', ({strapi}) => ({
-    async findOne(ctx) {
-        const {slug} = ctx.params;
-        const entity = await strapi.db.query('api::service.service').findOne({where: {slug},
-            populate: {image: true}});
-        const sanitizedEntity = await this.sanitizeOutput(entity);
-        return this.transformResponse(sanitizedEntity);
-    }
-}));
+module.exports = createCoreController('api::service.service');
--- src/api/skill/controllers/skill.js
+++ src/api/skill/controllers/skill.js
@@ -6,11 +6,4 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::skill.skill', ({strapi}) => ({
-    async findOne(ctx) {
-        const {slug} = ctx.params;
-        const entity = await strapi.db.query('api::skill.skill').findOne({where: {slug}});
-        const sanitizedEntity = await this.sanitizeOutput(entity);
-        return this.transformResponse(sanitizedEntity);
-    }
-}));
+module.exports = createCoreController('api::skill.skill');
